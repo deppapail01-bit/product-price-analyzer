32fcec34e72b9e58fbc2b1944df4cc2dbdeffd81
 Δέσποινα <despoina@Despoinas-MacBook.local>
2026-06-30 12:40:05 +0300

    Convert scraped data to pandas DataFrame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 url = 'https://books.toscrape.com'
 
 response = requests.get(url)
@@ -32,4 +33,5 @@ for book in books:
 
     books_data.append(book_data)
 
-    print(books_data)
+df = pd.DataFrame(books_data)
+print(df)
